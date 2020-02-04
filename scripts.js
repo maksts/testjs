b477de5617e15a715320eb1084f051754e985f07
Maksim <maxim.chelpanov@gmail.com>
2020-02-04 23:28:07 +0200

    First commit

---
+++ scripts.js
@@ -0,0 +1,65 @@
+'use strict';
+
+console.log(4/0); //
+
+
+console.log(4/'string');
+console.log('moved to the separate file');
+
+
+let leftBorderColor;
+console.log(leftBorderColor);
+
+
+
+let dog = {
+    name: "Barnie",
+    type: "French Bulldog",
+    age: 2,
+    color: "blue",
+    barking: true
+};
+
+console.log(dog.name);
+
+console.log(dog["color"]);
+
+
+let family = ['mama','papa','jana','david'];
+
+
+console.log(family[3]);
+
+//alert("Hello world!");
+
+//let answer = confirm("Are you sure?");
+
+//console.log(answer);
+
+
+let answer = prompt("Are you sure?", "yes");
+
+console.log(typeof(family));
+
+
+
+var x = 3; ++x - ++x - ++x;
+
+console.log(x);
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+
+console.log("increased 10 + 1 =" + ++incr);
+console.log("decreased 10 - 1 =" + --decr);
+
+console.log(5%2);
+
+
+test = 12;
+
+console.log(test);
---
+++ xtest.js
@@ -0,0 +1,14 @@
+
+var x = 3; 
+    
+var answer = ++x - ++x - ++x;
+
+
+console.log(answer);
+
+console.log(x);
+
+console.log(++x - ++x - ++x);
+
+console.log(x);
+
